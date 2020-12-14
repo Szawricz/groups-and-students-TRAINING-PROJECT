79a01601a4da9cc3e32afd5706c33eedc77e0c7b
Shavrin Maksim <nutmegraw@gmail.com>
2020-12-14 14:25:06 +0500

    Groups volume function added

--- models.py
+++ models.py
@@ -1,6 +1,29 @@
 """The models module."""
 
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, ForeignKey, Integer, MetaData, String, Table
+from sqlalchemy.orm import mapper
+
+metadata = MetaData()
+groups_table = Table(
+    'group', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String),
+)
+students_table = Table(
+    'student', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('group_id', String),
+    Column('first_name', String),
+    Column('last_name', String),
+)
+courses_table = Table(
+    'course', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String),
+    Column('description', String),
+)
+
+
 
 class GroupModel(object):
     def __init__(self, name):
